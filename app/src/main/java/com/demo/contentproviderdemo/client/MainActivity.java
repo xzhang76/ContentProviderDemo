abf9b852b865d62c7435f3aa27f686e6c1a193ab
010473 <zhangxiaoting@htsc.com>
2019-03-20 19:42:34 +0800

    Init a BookProvider onCreate() run in main thread, other functions run binder threads pool.

--- app/src/main/java/com/demo/contentproviderdemo/client/MainActivity.java
+++ app/src/main/java/com/demo/contentproviderdemo/client/MainActivity.java
@@ -1,5 +1,6 @@
 package com.demo.contentproviderdemo.client;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Uri uri = Uri.parse("content://com.demo.contentproviderdemo.provider");
+        getContentResolver().query(uri, null, null, null, null);
+        getContentResolver().query(uri, null, null, null, null);
+        getContentResolver().query(uri, null, null, null, null);
     }
 }
