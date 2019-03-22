8109545fa4f307cbb050975abab3698943f524f8
010473 <zhangxiaoting@htsc.com>
2019-03-22 16:58:52 +0800

    实现client的调用

---
+++ app/src/main/java/com/demo/contentproviderdemo/client/Book.java
@@ -0,0 +1,42 @@
+package com.demo.contentproviderdemo.client;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
+    public String mBookName = "";
+    public String mBookAuthor = "";
+
+    public Book(String name, String author) {
+        this.mBookName = name;
+        this.mBookAuthor = author;
+    }
+
+    protected Book(Parcel in) {
+        mBookName = in.readString();
+        mBookAuthor = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mBookName);
+        dest.writeString(mBookAuthor);
+    }
+}
--- app/src/main/java/com/demo/contentproviderdemo/client/MainActivity.java
+++ app/src/main/java/com/demo/contentproviderdemo/client/MainActivity.java
@@ -1,20 +1,65 @@
 package com.demo.contentproviderdemo.client;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.demo.contentproviderdemo.R;
+import com.demo.contentproviderdemo.provider.BookProvider;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BookRecyclerViewAdapter mAdapter;
+    private List<Book> mBooks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Uri uri = Uri.parse("content://com.demo.contentproviderdemo.provider");
-        getContentResolver().query(uri, null, null, null, null);
-        getContentResolver().query(uri, null, null, null, null);
-        getContentResolver().query(uri, null, null, null, null);
+
+        findViewById(R.id.add_book).setOnClickListener(this);
+        findViewById(R.id.get_books).setOnClickListener(this);
+
+        mAdapter = new BookRecyclerViewAdapter(mBooks);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_books);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        layoutManager.setOrientation(OrientationHelper.VERTICAL);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.add_book) {
+            ContentValues values = new ContentValues();
+            values.put("_id", 0);
+            values.put("name", "Android开发艺术探索");
+            getContentResolver().insert(BookProvider.BOOK_CONTENT_URI, values);
+
+        } else if (id == R.id.get_books) {
+            if (mBooks == null) {
+                mBooks = new ArrayList<>();
+            } else {
+                mBooks.clear();
+            }
+            Cursor bookCursor = getContentResolver().query(BookProvider.BOOK_CONTENT_URI, new String[]{"_id", "name"}, null, null, null);
+            while (bookCursor.moveToNext()) {
+                Book book = new Book(bookCursor.getString(1), bookCursor.getInt(0) + "");
+                mBooks.add(book);
+            }
+            mAdapter.updateBooks(mBooks);
+        }
     }
 }
